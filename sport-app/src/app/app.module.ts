183969f0dd25234cbf18e227b68e3956ba384c8a
Zack Klodnicki <klodzack@udel.edu>
2018-10-16 10:24:18 -0400

    Routing

--- sport-app/src/app/app.module.ts
+++ sport-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ListOfApisComponent } from './components/list-of-apis/list-of-apis.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ListOfApisComponent } from './components/list-of-apis/list-of-apis.comp
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
