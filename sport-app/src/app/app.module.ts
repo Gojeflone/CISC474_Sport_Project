400232a6e5bc8b86a80c6bcc1a8a87c544281224
jvillacis <juanfv@udel.edu>
2018-10-18 10:29:37 -0400

    Added filtering in search bar

--- sport-app/src/app/app.module.ts
+++ sport-app/src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule }   from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { ListOfApisComponent } from './components/list-of-apis/list-of-apis.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FilterPipe} from './components/list-of-apis/filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListOfApisComponent
+    ListOfApisComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
